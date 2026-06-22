bdd7cc1e14226f59407a51808cb0fc0fe36c8c63
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-22 20:27:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
